ab6412822aea10268b4ea9ccbeae6b6aa84a4b59
kush2863 <70647570+kush2863@users.noreply.github.com>
2024-01-28 14:08:51 +0530

    made the layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Events from './Components/Events/Events'
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Events />
+    </div>
   )
 }
 
---
+++ src/Components/Events/Events.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from './assets/Image1.svg'
+import Image2 from './assets/image2.svg'
+import './Events.css'
+
+const Events = () => {
+  return (
+    <>
+    <div className='events'>
+    <h1>Upcoming Events</h1>
+    <img  className='image1' src= {Image} alt='image' /> 
+    <div className='text-box1'>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </div>
+   
+    <img className='image2' src= {Image2} alt='image' /> 
+    <div className='text-box2'>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </div>
+   
+   </div>
+    </>
+  )
+}
+
+export default Events
