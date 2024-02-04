8d3db179c533de838d24d5fd88e481d069496329
Egon15 <pradneshtilekar15@gmail.com>
2024-02-04 15:50:39 +0530

    Animation Added

--- src/Components/Aboutus/Aboutus.js
+++ src/Components/Aboutus/Aboutus.js
@@ -1,15 +1,45 @@
-import "./Aboutus.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import "./About.css";
 
-const Aboutus = () => {
-  const [theme, setTheme] = useState("dark-theme");
+const App = () => {
+  const [theme, setTheme] = useState("light-theme");
 
   useEffect(() => {
     document.body.className = theme;
+
+    const handleScroll = () => {
+      const abtPara = document.querySelector(".abtPara span");
+      const scrollPosition = window.scrollY + window.innerHeight;
+      if (
+        abtPara.offsetTop < scrollPosition &&
+        abtPara.offsetTop + abtPara.offsetHeight > window.scrollY
+      ) {
+        abtPara.style.animationPlayState = "running";
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, [theme]);
 
   return (
     <>
+      <div className="App_container">
+        <div className="abtDiv">
+          <span className="abtTitle" data-text="About Us">
+            About Us
+          </span>
+          <p className="abtPara">
+            <span>
+              PCCOE&R ACM Student Chapter has been founded for the technical
+              development of students in the field of computing. The chapter
+              provides a platform for the students to showcase their talents.
+            </span>
+          </p>
+        </div>
+      </div>
       <button
         onClick={() => {
           setTheme(theme === "dark-theme" ? "light-theme" : "dark-theme");
@@ -17,19 +47,8 @@ const Aboutus = () => {
       >
         Change Mode
       </button>
-      <div className="App_container">
-        <div className="abtDiv">
-          <span className="abtTitle" data-text="About Us">
-            About Us
-          </span>
-          <p className="abtPara">
-            PCCOE&R ACM Student Chapter has been founded for the technical
-            development of students in the field of computing. The chapter
-            provides a platform for the students to showcase their talents.
-          </p>
-        </div>
-      </div>
     </>
   );
 };
-export default Aboutus;
+
+export default App;
