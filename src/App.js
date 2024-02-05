816dda486f5d6948226d5053d169173f4f19322b
Pranjal65 <pranjal.more_comp21@pccoer.in>
2024-02-05 19:46:44 +0530

    Revert "navabr fixed"
    This reverts commit 834d30bf1b63b7a1c035a8d5dfbfa285ea72a616.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Events from './Components/Events/Events'
 import './App.css'
+import Home from "./Components/Home/Home"
 import Navbar from './Components/Navbar/Navbar';
-
+import Aboutus from './Components/Aboutus/Aboutus';
+import Team from './Components/Team/Team';
+import Blogs from './Components/Blogs/Blogs';
 const App = () => {
   return (
-    <>
-      <Navbar/>
-    </>
+    <Router>
+    <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Events" element ={<Events />} />
+      <Route path="/About" element ={<Aboutus />} />
+      <Route path="/Team" element ={<Team />} />
+      <Route path="/Blogs" element ={<Blogs />} />
+      
+      </Routes>
+    </div>
+     
+</Router>
   )
 }
 
