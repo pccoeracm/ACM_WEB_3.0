6953ffe31abc13faaee32c0b3184d77b1ab7b714
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-02-05 22:05:40 +0530

    >

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Events from './Components/Events/Events'
 import './App.css'
-import Home from "./Components/Home/Home"
 import Navbar from './Components/Navbar/Navbar';
 
-import Blogs from './Components/Blogs/Blogs'
+
 const App = () => {
   return (
     <>
-      <Navbar/>
-      
+      <Navbar/>   
     </>
   )
 }
