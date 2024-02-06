54351f3636e7c41f0f601fe7121e1f6f6c10a695
esha023 <eshachavan05@gmail.com>
2024-02-06 21:22:47 +0530

    Home-page w/o mode

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,39 +1,18 @@
-import React, {useState}from 'react';
+import React from 'react';
 import gif from '../Assets/giphy.gif';
 import './Home.css';
 const Home = ()=>
 {
-    const [mode, setmode] = useState({
-        color : "#001A2B",
-        backgroundColor : "white"
-    })
-
-    const Changemode = () =>{
-        if(mode.color === "rgb(199, 199, 199)"){
-            setmode({
-                color: "#001A2B",
-                backgroundColor: "white"
-            })
-        }
-        else{
-            setmode({
-                color: "rgb(199, 199, 199)",
-                backgroundColor : "#001A2B"
-            })      
-        }
-    }
-
     return(     
         <>
-            <div className='Home'style={mode}>
-                <button onClick={Changemode}>Mode</button>
-                <div className='header' style={mode} >
+            <div className='Home'>
+                <div className='header' >
                     <span className='h1'>Advancing </span>
                     <span className='h2'>Computing </span>
                     <span className='h3'>as</span>  
                 </div>
                 
-                <div className='Gif-text' style={mode}>
+                <div className='Gif-text'>
                     <span className="gif" >
                         <img src = {gif} className = "gif-img"alt = "Gif" preload= "auto"/>
                     </span>  
