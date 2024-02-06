f00b71fbc6a2a0616302036085de1fda1c5acaa4
esha023 <eshachavan05@gmail.com>
2024-02-06 21:16:15 +0530

    Home page updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,8 @@
 import React, {useState}from 'react';
 import gif from '../Assets/giphy.gif';
 import './Home.css';
-const Home = ()=>{
+const Home = ()=>
+{
     const [mode, setmode] = useState({
         color : "#001A2B",
         backgroundColor : "white"
@@ -21,21 +22,27 @@ const Home = ()=>{
             })      
         }
     }
+
     return(     
         <>
             <div className='Home'style={mode}>
                 <button onClick={Changemode}>Mode</button>
-                <span className='header'>
-                    <h1 style={mode}>Advancing Computing as </h1>
-                </span>
+                <div className='header' style={mode} >
+                    <span className='h1'>Advancing </span>
+                    <span className='h2'>Computing </span>
+                    <span className='h3'>as</span>  
+                </div>
                 
                 <div className='Gif-text' style={mode}>
-                    <span className="gif">
-                        <img src = {gif} className = "gif-img"alt = "Gif" />
+                    <span className="gif" >
+                        <img src = {gif} className = "gif-img"alt = "Gif" preload= "auto"/>
                     </span>  
-                    <span className="Lines" style={{paddingLeft: "4vw", fontSize: "4vw", fontWeight: "bold"}}>
-                        as Science and Profession
-                    </span>
+                    <div className="Lines">
+                         <span className='l1'>as </span>
+                         <span className='l2'>Science </span>
+                         <span className='l3'>and </span>
+                         <span className='l4'>Profession </span>  
+                    </div>
                 </div>
             </div>   
 
