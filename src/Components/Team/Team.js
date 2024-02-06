2caa92cc457eb8ebab92c02fb22f5577bb81c1a1
sevilOO <kalesahil565@gmail.com>
2024-02-06 23:58:18 +0530

    added team section

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,8 +1,150 @@
 import React from 'react'
+import './Team.css'
 
 const Team = () => {
   return (
-    <div></div>
+    <>
+      <div className="heading">
+        <h2>Our Team</h2>
+      </div>
+      <div className="team-cards">
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src="https://images.unsplash.com/photo-1707131001327-00916e33e1a4?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <h2 className="hero-title">Gaurav Latpate</h2>
+          <h4 className="hero-spec">Design Head</h4>
+          <div className="socials">
+            <a href="#">
+              <i className='bx bxl-twitter'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-instagram-alt'></i>
+            </a>
+            <a href="#">
+              <i className='bx bxl-linkedin-square'></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
